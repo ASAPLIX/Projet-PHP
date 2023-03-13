fc242cc698209f1d7a9c692879b1b39eb2457a1f
DevPilou <pierre-louis.chevreul@supinfo.com>
2023-03-13 10:17:12 +0100

    Big Push
    Architecture, header, login en cours

---
+++ js/header.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('#menuToggler').click(function(){
+        $(this).toggleClass('active');
+        $('#menu').toggleClass('active');
+    })
+})
---
+++ pages/login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../css/style.css">
+        <title>Flixup+ - Login</title>
+    </head>
+    <body>
+        <?php include '../include/header.html';?>
+        <main>
+            <form action="#" method="POST">
+                <h2>Login</h2>
+                <input type="text" placeholder="Username">
+                <input type="password" placeholder="Password">
+                <input type="submit" value="Login">
+            </form>
+            <form action="#" method="POST">
+                <h2>Sign In</h2>
+                <input type="text" placeholder="Name">
+                <input type="text" placeholder="First Name">
+                <input type="date" placeholder="Age">
+                <input type="email" placeholder="Email">
+                <input type="password" placeholder="Password">
+                <input type="submit" value="Sign Up">s
+            </form>
+        </main>
+    </body>
+</html>
