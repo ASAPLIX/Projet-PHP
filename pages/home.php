870624d2084b82250dafe6fea562f8b778f7136d
DevPilou <pierre-louis.chevreul@supinfo.com>
2023-03-13 22:39:24 +0100

    Fnal Push
    Just given up

--- pages/home.php
+++ pages/home.php
@@ -12,19 +12,19 @@ session_start();
     </head>
     <body>
         <?php include '../include/header.html'?>
-        <main>
-            <section class="recommandation">
-			    <h2>Recommandation du jour</h1>
+        <main id="mainMenu">
+            <section id="recommendation">
+			    <h2>Recommandation du jour</h2>
 				    <img src="../image/36835430cd66ff6068aef005e6603d32.jpg" alt="Film du jour">
-				    <h3>WAR SPACE: La menace Predator Fantôme</h2>
+				    <h3>WAR SPACE: La menace Predator Fantôme</h3>
 			    <p>Avant de devenir un célèbre chevalier Jedi, et bien avant de se révéler l'âme la plus noire de la galaxie, Le major Dutch Schaeffer, spécialisé dans les opérations de commando, est chargé de délivrer trois civils américains retenus par des rebelles en Amérique latine. Chemin faisant, entre les alligators et les lianes, Dutch et ses hommes découvrent qu'ils ont été abusés et qu'ils doivent en réalité détruire une base secrète des guérilleros.  La Force est déjà puissante en lui et il est un remarquable pilote de Podracer. Le maître Jedi Qui-Gon Jin le découvre et entrevoit alors son immense potentiel. Pendant ce temps, l'armée de droïdes de l'insatiable Fédération du Commerce a envahi Naboo dans le cadre d'un plan secret des Sith visant à accroître leur pouvoir.</p>
 		    </section>
-            <section class="Nouveautes">
-                <h1>Nouveautés</h1>
+            <section id="newcomer">
+                <h2>Nouveautés</h2>
                 <ul>
                     <div>
                         <img src="../image/84842292.jpg" alt="Nouveautés 1">
-                        <h2>Metal Man</h2>
+                        <h3>Metal Man</h3>
                     </div>   
                 </ul>
             </section>
