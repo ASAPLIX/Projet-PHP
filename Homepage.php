53de66a3335a0176dbca7505ee867e8e4d004163
ASAPLIX <118887966+ASAPLIX@users.noreply.github.com>
2023-03-13 16:32:41 +0100

    Push homepage

---
+++ Homepage.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="IE-edge">
+        <link rel="stylesheet" href="Homepage.css">
+        <title>Page d'acceuil</title>
+    </head>
+    <body>
+        <?php include 'header.html'?>
+        <section class="recommandation">
+			<h1>Recommandation du jour</h1>
+				<img src="36835430cd66ff6068aef005e6603d32.jpg" alt="Film du jour">
+				<h2>WAR SPACE: La menace Predator Fantôme</h2>
+			<p>Avant de devenir un célèbre chevalier Jedi, et bien avant de se révéler l'âme la plus noire de la galaxie, Le major Dutch Schaeffer, spécialisé dans les opérations de commando, est chargé de délivrer trois civils américains retenus par des rebelles en Amérique latine. Chemin faisant, entre les alligators et les lianes, Dutch et ses hommes découvrent qu'ils ont été abusés et qu'ils doivent en réalité détruire une base secrète des guérilleros.  La Force est déjà puissante en lui et il est un remarquable pilote de Podracer. Le maître Jedi Qui-Gon Jin le découvre et entrevoit alors son immense potentiel. Pendant ce temps, l'armée de droïdes de l'insatiable Fédération du Commerce a envahi Naboo dans le cadre d'un plan secret des Sith visant à accroître leur pouvoir.</p>
+			<button>Ajouter au panier</button>
+		</section>
+        <section class="Nouveautes">
+            <h1>Nouveautés</h1>
+            <ul>
+                <div>
+                    <img src="84842292.jpg" alt="Nouveautés 1">
+                    <h2>Metal Man</h2>
+                </div>   
+            </ul>
+        </section>
+    </body>
+</html>
